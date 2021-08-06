62a14158ff8c677fe48404d75e4b58960d097233
Will Fisher <wfisher@chef.io>
2021-08-06 14:51:10 -0700

    Implement check for failed shards and corrupted translogs
    Signed-off-by: Will Fisher <wfisher@chef.io>

--- chef-server/controls/elasticsearch.rb
+++ chef-server/controls/elasticsearch.rb
@@ -1,4 +1,5 @@
 es_cluster_state = log_analysis('elasticsearch_cluster_state.txt')
+es_logs = log_analysis('var/log/opscode/elasticsearch/current')
 
 # read-only indices
 control 'gatherlogs.chef-server.elasticsearch_read_only_indicies' do
@@ -19,4 +20,55 @@ control 'gatherlogs.chef-server.elasticsearch_read_only_indicies' do
     its('last_entry') { should be_empty }
   end
   tag summary: es_cluster_state.summary!
+end
+
+# primary shard is not active
+control 'gatherlogs.automate2.elasticsearch_failed shards' do
+  title 'Check to see if Elasticsearch is reporting issues with failed shards'
+  desc "
+Elasticsearch is reporting that there are some shards are unavailable.
+
+To attempt a retry for the shards, issue the following
+
+    curl -XPOST 'localhost:9200/_cluster/reroute?retry_failed&pretty'
+
+If that gives an error saying the shard is already assigned, then you will need to issue a flush to clear
+the sync ID per each associated index that you received the 'already assigned' error for, and then retry
+the above reroute command
+
+    curl -XPOST 'localhost:9200/INDEX_NAME/_flush?force=true&pretty'
+  "
+  describe es_logs.find('failed shard') do
+    its('last_entry') { should be_empty }
+  end
+  describe es_logs.find('primary shard is not active') do
+    its('last_entry') { should be_empty }
+  end
+  describe es_logs.find('org.elasticsearch.action.search.SearchPhaseExecutionException: all shards failed') do
+    its('last_entry') { should be_empty }
+  end
+  tag summary: es_logs.summary!
+end
+
+control 'gatherlogs.chef-server.elasticsearch_corrupted_translog' do
+  title 'Check to see if there are any errors about corrupted transaction logs'
+  desc "
+Elasticsearch is reporting errors for possibly truncated or corrupted transaction
+logs.  This can happen if there was a disk full event that occured or if the ES
+service was unexpectedly terminated.
+
+To resolve this:
+1. Stop Chef Infra Server services: chef-server-ctl stop
+2. Fix/Remove the bad transaction log file
+3. Start the services again: chef-server-ctl start
+  "
+
+  tag kb: [
+    'https://www.elastic.co/guide/en/elasticsearch/reference/6.8/shard-tool.html'
+  ]
+
+  describe es_logs.find('TranslogCorruptedException') do
+    its('last_entry') { should be_empty }
+  end
+  tag summary: es_logs.summary!
 end
